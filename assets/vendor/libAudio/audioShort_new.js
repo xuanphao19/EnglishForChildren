2a840ccd1bd7a544d23b09d8c889fa65aa2434dc
Xuan Phao <nguyenthanhhoa075@gmail.com>
2023-05-08 20:10:47 +0700

    swiper initialized

---
+++ assets/vendor/libAudio/audioShort_new.js
@@ -0,0 +1,395 @@
+var prev_element = null;
+var style_box_nonvip =
+  "<style>.box_alert_vip{background-image: url(/file/common/libAudio/images/vip_bg.png);color: #fff;display: none;position: fixed;bottom: 3px;z-index: 9999;border-radius: 8px;background-color: #ee7600;background-repeat: no-repeat;width: 94%;left: 3%;height: 70px;padding:0px;}.box_vip_close {background: url(/file/common/libAudio/images/close.png) no-repeat;height: 22px;width: 22px;position: absolute;top: 10px;right: 10px;cursor: pointer;}</style>";
+var serv_curr = window.location.protocol + "//" + location.host;
+if (serv_curr == "https://www.tienganh123.com" || serv_curr == "http://www.tienganh123.com") {
+  style_box_nonvip =
+    "<style>#box_audio_nonvip{background-image: url(/file/common/libAudio/images/vip_bg.png);color: #fff;display: none;position: fixed;bottom: 3px;z-index: 9999;border-radius: 8px;background-color: #ee7600;background-repeat: no-repeat;width: 30%;left: 30%;height: 50px;padding:0px;}.box_vip_close {background: url(/file/common/libAudio/images/close.png) no-repeat;height: 22px;width: 22px;position: absolute;top: 10px;right: 10px;cursor: pointer;}</style>";
+}
+var str_nonvip =
+  style_box_nonvip +
+  '<div id="box_audio_nonvip" class="box_alert_vip">\
+<div class="box_vip_text" style="margin:7px 15px 0px 15px">Báº¡n pháº£i lÃ  <a href="/huong-dan/214-quyen-loi-thanh-vien-vip-cua-tienganh123.html" target="_blank" title="quyá»n lá»£i thÃ nh viÃªn VIP" style="color:#F2FF32; text-decoration:underline; font-weight:bold">thÃ nh viÃªn VIP</a> cá»§a Tiáº¿ngAnh123.Com <br>má»›i nghe Ä‘Æ°á»£c pháº§n nÃ y. <a href="/huong-dan/4668-cach-dat-the-vip-tren-tienganh123.html" target="_blank" title="ÄÄƒng kÃ½ thÃ nh viÃªn VIP" style="color:#F2FF32; text-decoration:underline; font-weight:bold">ÄÄƒng kÃ½ thÃ nh viÃªn VIP</a></div><div class="box_vip_close"></div></div>';
+var str_nonkid =
+  style_box_nonvip +
+  '<div id="box_audio_nonvip" class="box_alert_vip">\
+<div class="box_vip_text" style="margin:7px 15px 0px 15px">Báº¡n pháº£i lÃ  <a href="/huong-dan/214-quyen-loi-thanh-vien-vip-cua-tienganh123.html" target="_blank" title="quyá»n lá»£i thÃ nh viÃªn VIP" style="color:#F2FF32; text-decoration:underline; font-weight:bold">thÃ nh viÃªn VIP</a> cá»§a Tiáº¿ngAnh123.Com <br>má»›i nghe Ä‘Æ°á»£c pháº§n nÃ y. <a href="/huong-dan/4668-cach-dat-the-vip-tren-tienganh123.html" target="_blank" title="ÄÄƒng kÃ½ thÃ nh viÃªn VIP" style="color:#F2FF32; text-decoration:underline; font-weight:bold">ÄÄƒng kÃ½ thÃ nh viÃªn VIP</a></div><div class="box_vip_close"></div></div>';
+var lesson_vip = 0,
+  str_nonmember = 0;
+function PlayingAudioHide(element) {
+  if (element.hasClass("uba_hideAudio")) {
+    element
+      .children(".stt_par_hideaudio")
+      .children(0)
+      .attr("src", "//noidung.tienganh123.com/file/common/libAudio/images/speaker-playing.gif");
+    if (prev_element != null) {
+      if (element.attr("media-url") != prev_element.attr("media-url")) {
+        stopAudioHide(prev_element);
+      }
+    }
+    prev_element = element;
+  }
+}
+function stopAudioHide(element) {
+  if (element.hasClass("uba_hideAudio")) {
+    element
+      .children(".stt_par_hideaudio")
+      .children(0)
+      .attr("src", "//noidung.tienganh123.com/file/common/libAudio/images/speaker-stop.gif");
+  }
+}
+(function ($) {
+  var defaults = {
+      audioButtonClass: "uba_audioButton",
+      autoPlay: null,
+      codecs: [
+        {
+          name: "OGG",
+          codec: 'audio/ogg; codecs="vorbis"',
+        },
+        {
+          name: "MP3",
+          codec: "audio/mpeg",
+        },
+      ],
+      continuous: false,
+      extension: null,
+      flashExtension: ".mp3",
+      flashObjectID: "audioPlayer",
+      loadingClass: "loading",
+      loop: false,
+      playerContainer: "player",
+      playingClass: "playing",
+      onEndClass: "on-ended-audio",
+      playStartCallback: null,
+      stopCallback: null,
+      isPaid: 1,
+      n_play: 0,
+      volume: 1,
+    },
+    audio,
+    currentTrack,
+    isPlaying,
+    index_audio,
+    curr_audio,
+    audio_s = [],
+    $buttons_s,
+    $tgt,
+    $el,
+    playTrack,
+    resumeTrack,
+    pauseTrack,
+    methods = {
+      play: function (element, i) {
+        $("#box_audio_nonvip").removeAttr("style");
+        $(".box_alert_vip").hide();
+        if (!defaults.isPaid && defaults.n_play > 2) {
+          currentTrack = "https://data.tienganh123.com/file/dungchung/vip_only_audio.mp3";
+          str_nonmember = str_nonvip;
+        } else {
+          currentTrack = element.attr("media-url");
+        }
+        element.attr("media-url", currentTrack);
+        audio_s[i] = new Audio("");
+        audio_s[i].id = "audio_" + i;
+        audio_s[i].loop = defaults.loop ? "loop" : "";
+        audio_s[i].volume = defaults.volume;
+        audio_s[i].src = currentTrack;
+        _methods.addListeners(audio_s[i]);
+        audio_s[i].play();
+        isPlaying = true;
+      },
+      play_app: function (element) {
+        $("#box_audio_nonvip").removeAttr("style");
+        currentTrack = element.attr("media-url");
+        if (currentTrack.indexOf(" ") >= 0) {
+          currentTrack = currentTrack.replace(/ /g, "%20");
+        }
+        element.attr("media-url", currentTrack);
+        if (audio_s[0]) {
+          audio_s[0].pause();
+          audio_s[0].src = currentTrack;
+        } else {
+          audio_s[0] = new Audio("");
+          audio_s[0].id = "audio_s_";
+          audio_s[0].loop = defaults.loop ? "loop" : "";
+          audio_s[0].volume = defaults.volume;
+          audio_s[0].src = currentTrack;
+        }
+        _methods.addListeners(audio_s[0]);
+        audio_s[0].play();
+        isPlaying = true;
+      },
+      pause: function (element) {
+        index_audio = element.index("." + defaults.audioButtonClass);
+        audio_s[index_audio].pause();
+        isPlaying = false;
+        element.removeClass(defaults.playingClass).addClass("audio_pause_uba");
+        if (typeof defaults.stopCallback == "function") {
+          defaults.stopCallback($tgt);
+        }
+      },
+      pause_app: function () {
+        isPlaying = false;
+        audio_s[0].pause();
+        $("." + defaults.playingClass).removeClass(defaults.playingClass);
+        if (typeof defaults.stopCallback == "function") {
+          defaults.stopCallback($tgt);
+        }
+      },
+      pause_other: function (element) {
+        isPlaying = false;
+        index_audio = element.index("." + defaults.audioButtonClass);
+        audio_s[index_audio].pause();
+        element.removeClass(defaults.playingClass);
+        if (typeof defaults.stopCallback == "function") {
+          defaults.stopCallback($tgt);
+        }
+        currentTrack = element.attr("media-url");
+        audio_s[index_audio].currentTime = 0;
+      },
+      resume: function (element) {
+        isPlaying = true;
+        index_audio = element.index("." + defaults.audioButtonClass);
+        _methods.addListeners(audio_s[index_audio]);
+        if (element.find(".stt_par_hideaudio").length == 1) {
+          audio_s[index_audio].currentTime = 0;
+        }
+        audio_s[index_audio].play();
+
+        element.addClass(defaults.playingClass);
+        if (typeof defaults.playStartCallback == "function") {
+          defaults.playStartCallback(element);
+        }
+      },
+      resume_app: function () {
+        isPlaying = true;
+        _methods.addListeners(audio_s[0]);
+        if ($tgt.find(".stt_par_hideaudio").length == 1) {
+          audio_s[0].currentTime = 0;
+        }
+        audio_s[0].play();
+        $tgt.addClass(defaults.playingClass);
+        if (typeof defaults.playStartCallback == "function") {
+          defaults.playStartCallback($tgt);
+        }
+      },
+      resume_other: function (element) {
+        isPlaying = true;
+        index_audio = element.index("." + defaults.audioButtonClass);
+        _methods.addListeners(audio_s[index_audio]);
+        audio_s[index_audio].src = element.attr("media-url");
+        audio_s[index_audio].play();
+        element.addClass(defaults.playingClass);
+      },
+    },
+    _methods = {
+      init: function (options) {
+        //set defaults
+        $.extend(defaults, options);
+        $el = this;
+        $buttons_s = $("." + defaults.audioButtonClass);
+        //listen for clicks on the controls
+        $buttons_s.unbind("click");
+        if ((typeof fromapp != "undefined" && fromapp) || (typeof checkApp == "function" && checkApp())) {
+          $buttons_s.bind("click", function (i) {
+            _methods.updateTrackState_app(this);
+          });
+        } else {
+          $buttons_s.bind("click", function (i) {
+            var inx_audio = $(this).index("." + defaults.audioButtonClass);
+            _methods.updateTrackState(this, inx_audio);
+          });
+        }
+        if (defaults.autoPlay) {
+          methods.play(defaults.autoPlay);
+        }
+        types = defaults.codecs;
+        for (var i = 0, ilen = types.length; i < ilen; i++) {
+          var type = types[i];
+          if (_methods.canPlay(type)) {
+            defaults.extension = [".", type.name.toLowerCase()].join("");
+            break;
+          }
+        }
+        if (defaults.autoPlay) {
+          methods.play(defaults.autoPlay);
+        }
+      },
+
+      updateTrackState: function (element, i) {
+        $tgt = $(element);
+        if (!$tgt.hasClass(defaults.audioButtonClass)) {
+          return;
+        }
+        var inx_audio_pl = $("." + defaults.playingClass).index("." + defaults.audioButtonClass);
+        var inx_audio_onended = $("." + defaults.onEndClass).index("." + defaults.audioButtonClass);
+        if (inx_audio_pl != i) defaults.n_play++;
+        if ($("." + defaults.playingClass).length > 0 && inx_audio_pl != i) {
+          methods.pause_other($("." + defaults.playingClass));
+          $("." + defaults.playingClass).removeClass(defaults.playingClass);
+        }
+        var inx_audio_pl_ = $(".audio_pause_uba").index("." + defaults.audioButtonClass);
+        if ($(".audio_pause_uba").length > 0 && inx_audio_pl_ != i) {
+          methods.pause_other($(".audio_pause_uba"));
+          $(".audio_pause_uba").removeClass("audio_pause_uba");
+        }
+        var media_u = $tgt.attr("media-url");
+        if (media_u.indexOf(" ") >= 0) {
+          media_u = media_u.replace(/ /g, "%20");
+        }
+        var file_name = media_u.split("/");
+        var n_fn = file_name.length - 1;
+        if (!audio_s[i] || (audio_s[i] && audio_s[i].src.indexOf("/" + file_name[n_fn]) < 0)) {
+          $tgt.addClass(defaults.loadingClass);
+          methods.play($tgt, i);
+        } else {
+          if ($tgt.find(".stt_par_hideaudio").length == 1) {
+            methods.pause($tgt);
+            methods.resume($tgt);
+          } else {
+            if (isPlaying) {
+              methods.pause($tgt);
+            } else {
+              methods.resume($tgt);
+            }
+          }
+        }
+      },
+      updateTrackState_app: function (element) {
+        defaults.n_play++;
+        $tgt = $(element);
+        var media_u = $tgt.attr("media-url");
+        if (media_u.indexOf(" ") >= 0) {
+          media_u = media_u.replace(/ /g, "%20");
+        }
+        var file_name = media_u.split("/");
+        var n_fn = file_name.length - 1;
+
+        if (!$tgt.hasClass(defaults.audioButtonClass)) {
+          return;
+        }
+        if (!audio_s[0]) {
+          $tgt.addClass(defaults.loadingClass);
+          methods.play_app($tgt);
+        } else {
+          if (!isPlaying) {
+            if (currentTrack.indexOf(file_name[n_fn] + ".mp3") < 0) {
+              $tgt.addClass(defaults.loadingClass);
+              methods.play_app($tgt);
+            } else {
+              methods.resume_app();
+            }
+          } else {
+            methods.pause_app();
+            if (currentTrack.indexOf(file_name[n_fn] + ".mp3") < 0) {
+              $tgt.addClass(defaults.loadingClass);
+              methods.play_app($tgt);
+            }
+          }
+        }
+      },
+
+      addListeners: function (elem) {
+        $(elem).bind({
+          canplay: _methods.onLoaded,
+          error: _methods.onError,
+          ended: _methods.onEnded,
+        });
+      },
+
+      removeListeners: function (elem) {
+        $(elem).unbind({
+          canplay: _methods.onLoaded,
+          error: _methods.onError,
+          ended: _methods.onEnded,
+        });
+      },
+
+      onLoaded: function () {
+        $buttons_s.removeClass(defaults.loadingClass);
+        $tgt.addClass(defaults.playingClass);
+        if (typeof defaults.playStartCallback == "function") {
+          defaults.playStartCallback($tgt);
+        }
+      },
+
+      onError: function () {
+        $buttons_s.removeClass(defaults.loadingClass);
+        _methods.removeListeners(audio_s[index_audio]);
+      },
+      onEnded: function () {
+        isPlaying = false;
+        $tgt.removeClass(defaults.playingClass);
+        $tgt.addClass(defaults.onEndClass);
+        if (typeof defaults.stopCallback == "function") {
+          defaults.stopCallback($tgt);
+        }
+
+        _methods.removeListeners(audio_s[index_audio]);
+        if (defaults.continuous) {
+          var $next = $tgt.next().length ? $tgt.next() : $(defaults.audioButtonClass).eq(0);
+          methods.play($next);
+        }
+      },
+
+      canPlay: function (type) {
+        if (!document.createElement("audio").canPlayType) {
+          return false;
+        } else {
+          return document
+            .createElement("audio")
+            .canPlayType(type.codec)
+            .match(/maybe|probably/i)
+            ? true
+            : false;
+        }
+      },
+
+      swfLoaded: function () {
+        if (defaults.autoPlay) {
+          setTimeout(function () {
+            methods.play(defaults.autoPlay);
+          }, 500);
+        }
+      },
+
+      getFileNameWithoutExtension: function (fileName) {
+        var fileNamePieces = fileName.split(".");
+        fileNamePieces.pop();
+        return fileNamePieces.join(".");
+      },
+    };
+
+  $.fn.ubaPlayer = function (method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === "object" || !method) {
+      return _methods.init.apply(this, arguments);
+    } else {
+      $.error("Method " + method + " does not exist on jquery.ubaPlayer");
+    }
+  };
+})(jQuery);
+function PlayingAudioHide2(element) {
+  if (element.hasClass("uba_ctrl_game_hide")) {
+    element
+      .children("span")
+      .children(0)
+      .attr("src", "//noidung.tienganh123.com/file/common/libAudio/images/audio_playing_bt.gif");
+    if (prev_element != null) {
+      if (element.attr("media-url") != prev_element.attr("media-url")) {
+        stopAudioHide2(prev_element);
+      }
+    }
+    prev_element = element;
+  }
+}
+function stopAudioHide2(element) {
+  if (element.hasClass("uba_ctrl_game_hide")) {
+    element
+      .children("span")
+      .children(0)
+      .attr("src", "//noidung.tienganh123.com/file/common/libAudio/images/audio_stop_bt.gif");
+  }
+}
--- file/common/lesson/flashcard/vocab.js
+++ file/common/lesson/flashcard/vocab.js
@@ -6,104 +6,6 @@ function is_not_firefox() {
     return false;
   }
 }
-function load_css(link_css) {
-  var cre = document.createElement("link");
-  cre.type = "text/css";
-  cre.href = link_css;
-  cre.rel = "stylesheet";
-  var x = document.getElementsByTagName("head")[0];
-  x.appendChild(cre);
-}
-var basic_url = window.location.protocol + "//" + location.host + "/";
-var link_login = "/clogin",
-  link_post = "ajax/";
-if (typeof is_mobile != "undefined" || typeof is_tablet != "undefined") {
-  link_login = "/login";
-  link_post = "ajax/ajax/";
-}
-
-var obj_vbkd = {
-  arr_char: [
-    "q",
-    "w",
-    "e",
-    "r",
-    "t",
-    "y",
-    "u",
-    "i",
-    "o",
-    "p",
-    "a",
-    "s",
-    "d",
-    "f",
-    "g",
-    "h",
-    "j",
-    "k",
-    "l",
-    "z",
-    "x",
-    "c",
-    "v",
-    "b",
-    "n",
-    "m",
-  ],
-  post_url: window.location.protocol + "//" + location.host + "/",
-  id_game: "",
-  start_word: 0,
-  next_w: 0,
-  score: 0,
-  word_curr: [],
-  word_list: [],
-  number: 0,
-  list_again: [],
-  html_non_vip_kid: "",
-  is_mobile: 0,
-  is_tablet: 0,
-  loc: _loc,
-  saved: false,
-  list_alphabet: [
-    "q.mp3",
-    "w.mp3",
-    "e.mp3",
-    "r.mp3",
-    "t.mp3",
-    "y.mp3",
-    "u.mp3",
-    "i.mp3",
-    "o.mp3",
-    "p.mp3",
-    "a.mp3",
-    "s.mp3",
-    "d.mp3",
-    "f.mp3",
-    "g.mp3",
-    "h.mp3",
-    "j.mp3",
-    "k.mp3",
-    "l.mp3",
-    "z.mp3",
-    "x.mp3",
-    "c.mp3",
-    "v.mp3",
-    "b.mp3",
-    "n.mp3",
-    "m.mp3",
-  ],
-};
-
-var UNF = "undefined";
-function loadScript_item(link_js, id) {
-  var cre = document.createElement("script");
-  cre.type = "text/javascript";
-  cre.async = true;
-  cre.src = link_js;
-  var x = document.getElementById(id);
-  x.appendChild(cre);
-}
 var front = true;
 function flip_card(e) {
   $(e).flip({
@@ -116,11 +18,11 @@ function flip_card(e) {
       } else {
         $(e).find(".face").hide().next().show().find(".uba_audioButton:eq(0)").click();
       }
-      $(e).css("background-color", "rgb(248 248 190)");
       front = !front;
     },
   });
 }
+
 function loadScript_callback(url, callback) {
   $.getScript(url, function (data, textStatus, jqxhr) {
     if (jqxhr.status != 200) {
@@ -131,79 +33,50 @@ function loadScript_callback(url, callback) {
   });
 }
 
-var target_elm = null,
+let current_card = 0,
+  number_card = 0,
+  target_elm = null,
   all_vocab = false;
 
 $(document).ready(function () {
   if (typeof is_tablet != "undefined") $("#icon_audio_click").addClass("audio_tablet");
   number_card = $(".card_flip").length;
-  $(".item_card").click(function (event) {
+  $(".item_wrap").click(function (event) {
     target_elm = $(event.target);
-    if (!target_elm.is(".uba_audioButton") && !target_elm.is(".recording_user")) {
+    if (!target_elm.is(".uba_audioButton")) {
       flip_card(this);
     }
   });
-  loadScript_callback("https://www.tienganh123.com/file/common/libAudio/js/audioShort_new.js", function () {
+  loadScript_callback("./assets/vendor/libAudio/audioShort_new.js", function () {
     $("#ubaPlayer").ubaPlayer({
       codecs: [{name: "MP3", codec: "audio/mpeg;"}],
     });
   });
-  $(".recording_user").click(function () {
-    recordClick(this);
-  });
 });
-var count_word = 0;
 
-function card_back(e) {
-  front = true;
-  $(".card_next").show();
-  if (current_card == 1) {
-    $(".card_back").hide();
-  }
-  if (current_card > 0) {
-    current_card = current_card - 1;
-    var mg = $("#ul_card123").attr("left");
-    mg = +left_inc;
-    $("#ul_card123").animate(
-      {
-        left: "+=" + mg,
-      },
-      700,
-    );
-
-    $("#ta123_card_" + current_card + " .face")
-      .show()
-      .find(".uba_audioButton:eq(0)")
-      .click();
-    $("#ta123_card_" + current_card + " .back").hide();
-  }
-}
-var el = $(".card_flip").width();
-var current_card = 0,
-  number_card = 0,
-  left_inc = `${el}`;
-
-function card_next(e) {
-  front = true;
-  $(".card_back").show();
-  if (current_card == number_card - 1) {
-    $(".card_next").hide();
-  }
-  if (current_card < number_card - 1) {
-    current_card = current_card + 1;
-    var mg = $("#ul_card123").attr("left");
-    mg = -left_inc;
-    $("#ul_card123").animate(
-      {
-        left: "+=" + mg,
-      },
-      700,
-    );
-
-    $("#ta123_card_" + current_card + " .face")
-      .show()
-      .find(".uba_audioButton:eq(0)")
-      .click();
-    $("#ta123_card_" + current_card + " .back").hide();
-  }
-}
+const swiper = new Swiper(".hero_slider", {
+  spaceBetween: 5,
+  centeredSlides: true,
+  speed: 2000,
+  loop: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    init: function () {
+      console.log("swiper initialized");
+    },
+  },
+});
+swiper.on("slideChange", function () {
+  let currentEl,
+    currentIndex = swiper.realIndex;
+  currentEl = $(".ubaLetter")[`${currentIndex}`];
+  currentEl.click();
+});
